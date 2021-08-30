a92a38127bf85f6be299fab0be90dbac4a09fd2c
Sergey Zubrilin <serginhold.box@gmail.com>
2021-08-30 21:33:51 +0300

    filter attributes

--- src/Repository/Product/ProductRepository.php
+++ src/Repository/Product/ProductRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\Product;
 
 use App\Entity\Product\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -18,5 +19,31 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     * @param array $filter ['color' => ['red', 'blue']]
+     */
+    private function filterAttributes(QueryBuilder $builder, array $filter): void
+    {
+        $join = $builder->getRootAliases()[0].'.values';
+        $alias = 'product_values';
+        $builder->join($join, $alias);
+
+        foreach ($filter as $code => $values) {
+            if (!is_array($values)) {
+                $values = [$values];
+            }
+
+            $key = 'pv_' . hash('crc32c', $code);
+            $keyValues = $key . '_array';
+
+            $builder
+                ->andWhere("JSONB_EXISTS_ANY(JSONB_EXTRACT({$alias}.values, :{$key}), ARRAY_TEXT(:{$keyValues})) = true")
+                ->setParameter($key, $code)
+                ->setParameter($keyValues, $values)
+            ;
+        }
     }
 }
