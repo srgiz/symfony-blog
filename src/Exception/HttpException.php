944aae5ddc92791038dbc7925edaf559604a935a
Sergey Zubrilin <serginhold.box@gmail.com>
2021-08-30 23:06:49 +0300

    HttpException

---
+++ src/Exception/HttpException.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException as KernelHttpException;
+use Symfony\Component\Validator;
+
+class HttpException extends KernelHttpException implements DataExceptionInterface
+{
+    private ?array $data;
+
+    public function __construct(
+        int $statusCode,
+        ?string $message = null,
+        array $data = null,
+        \Throwable $previous = null,
+        array $headers = [],
+        ?int $code = null
+    ) {
+        parent::__construct(
+            $statusCode,
+            $message ?? Response::$statusTexts[$statusCode],
+            $previous,
+            $headers,
+            $code ?? $statusCode
+        );
+
+        $this->data = $data;
+    }
+
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    public function setData(?array $data): static
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function setDataValidatorErrors(Validator\ConstraintViolationListInterface $errors): static
+    {
+        $data = [];
+
+        /** @var Validator\ConstraintViolationInterface $error */
+        foreach ($errors as $error) {
+            $data[$error->getPropertyPath()][] = $error->getMessage();
+        }
+
+        return $this->setData($data);
+    }
+
+}
