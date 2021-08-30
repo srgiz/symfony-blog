7c58ed1e19d674949f400d2a961606f2d6e65c15
Sergey Zubrilin <serginhold.box@gmail.com>
2021-08-30 17:08:27 +0300

    doctrine ARRAY_TEXT() array[]::text[]

---
+++ src/Doctrine/Functions/ArrayText.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Doctrine\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * array[]::text[]
+ */
+class ArrayText extends FunctionNode
+{
+    /** @var Node[] */
+    private array $array = [];
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        $values = [];
+
+        foreach ($this->array as $node) {
+            $values[] = $node->dispatch($sqlWalker);
+        }
+
+        $count = count($values);
+
+        if (!$count) {
+            $format = '';
+        } else if ($count === 1) {
+            $format = '%s';
+        } else {
+            $format = str_repeat('%s, ', $count - 1) . '%s';
+        }
+
+        $format = 'array[' . $format . ']';
+
+        if (!$count) {
+            $format .= '::text[]';
+        }
+
+        return vsprintf($format, $values);
+    }
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        while ($parser->getLexer()->isNextToken(Lexer::T_STRING)) {
+            $this->array[] = $parser->StringPrimary();
+
+            if ($parser->getLexer()->isNextToken(Lexer::T_COMMA)) {
+                $parser->match(Lexer::T_COMMA);
+            }
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
