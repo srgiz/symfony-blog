115c268f2e8eea6c871ac7229cbc818056e9490d
Sergey Zubrilin <serginhold.box@gmail.com>
2021-12-04 21:33:03 +0300

    cache

--- src/Nav/SiteNavBuilder.php
+++ src/Nav/SiteNavBuilder.php
@@ -6,10 +6,13 @@ namespace App\Nav;
 use App\Dto\Nav\NavCollection;
 use App\Dto\Nav\NavItem;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class SiteNavBuilder implements SiteNavBuilderInterface
 {
     public function __construct(
+        private TagAwareCacheInterface $cache,
         private UrlGeneratorInterface $urlGenerator,
     ) {}
 
@@ -24,11 +27,16 @@ class SiteNavBuilder implements SiteNavBuilderInterface
 
     public function backend(): NavCollection
     {
-        $collection = new NavCollection();
+        return $this->cache->get('app_nav_backend', function (ItemInterface $item) {
+            $collection = new NavCollection();
 
-        $collection->add(new NavItem($this->urlGenerator->generate('backend_dashboard'), 'Dashboard'));
-        $collection->add(new NavItem($this->urlGenerator->generate('backend_dashboard'), 'Users'));
+            $collection->add(new NavItem($this->urlGenerator->generate('backend_dashboard'), 'Dashboard'));
+            $collection->add(new NavItem($this->urlGenerator->generate('backend_dashboard'), 'Users'));
 
-        return $collection;
+            //$item->tag(['app1']);
+            $item->expiresAfter(60);
+
+            return $collection;
+        });
     }
 }
