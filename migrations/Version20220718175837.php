ecc6bd66fd3cb41dbbce2cb300b623e5bf6f3ff6
Sergey Zubrilin <serginhold.box@gmail.com>
2022-07-19 13:38:02 +0300

    test func move node

--- migrations/Version20220718175837.php
+++ migrations/Version20220718175837.php
@@ -17,78 +17,100 @@ final class Version20220718175837 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql("
-        CREATE OR REPLACE FUNCTION add_category_tree(IN new_category_id integer, IN new_parent_id integer, IN new_parent_level integer) RETURNS void AS \$\$
-        BEGIN
-            INSERT INTO category_tree (category_id, child_id, level)
-            -- добавляем все категории внутри которых есть new_parent_id
-            SELECT category_id, new_category_id, /*new_parent_level*/level + 1
-            FROM category_tree
-            WHERE child_id = new_parent_id
-            UNION
-            -- добавляем ссылку на себя же
-            SELECT new_category_id, new_category_id, new_parent_level + 1
-            ;
-        END;
-        \$\$ LANGUAGE plpgsql
-        ");
-
-        $this->addSql("
-        CREATE OR REPLACE FUNCTION change_parent_category() RETURNS TRIGGER AS \$\$
+        CREATE OR REPLACE FUNCTION add_category_tree(IN NEW_CATEGORY_ID integer, IN NEW_PARENT_ID integer) RETURNS void AS \$\$
         DECLARE
-            old_parent_level integer;
             new_parent_level integer;
         BEGIN
             -- уровень вложенности нового родителя
             SELECT MAX(level) INTO new_parent_level FROM (
                 (SELECT 0 AS level)
                 UNION
-                (SELECT level FROM category_tree WHERE category_id = NEW.parent_id AND child_id = NEW.parent_id LIMIT 1)
+                (SELECT level FROM category_tree WHERE category_id = NEW_PARENT_ID AND child_id = NEW_PARENT_ID LIMIT 1)
             ) t;
             
-            IF (TG_OP = 'INSERT') THEN
-                EXECUTE add_category_tree(NEW.id, NEW.parent_id, new_parent_level);
-            ELSIF (TG_OP = 'UPDATE' AND COALESCE(OLD.parent_id, 0) != COALESCE(NEW.parent_id, 0)) THEN
-                -- запрещаем рекурсивную привязку друг к другу
-                IF (NEW.parent_id IN (SELECT child_id FROM category_tree WHERE category_id = NEW.id)) THEN
-                    RAISE EXCEPTION 'Category % is a child of category %', NEW.parent_id, NEW.id;
-                END IF;
-                
-                -- предыдущий уровень вложенности
-                SELECT MAX(level) INTO old_parent_level FROM (
-                    (SELECT 0 AS level)
-                    UNION
-                    (SELECT level FROM category_tree WHERE category_id = OLD.parent_id AND child_id = OLD.parent_id LIMIT 1)
-                ) t;
+            INSERT INTO category_tree (category_id, child_id, level)
+            -- добавляем все категории внутри которых есть NEW_PARENT_ID
+            SELECT category_id, NEW_CATEGORY_ID, /*new_parent_level*/level + 1
+            FROM category_tree
+            WHERE child_id = NEW_PARENT_ID
+            UNION
+            -- добавляем ссылку на себя же
+            SELECT NEW_CATEGORY_ID, NEW_CATEGORY_ID, new_parent_level + 1
+            ;
+        END;
+        \$\$ LANGUAGE plpgsql
+        ");
+
+        $this->addSql("
+        CREATE OR REPLACE FUNCTION move_category_tree(IN NEW_CATEGORY_ID integer, IN OLD_PARENT_ID integer, IN NEW_PARENT_ID integer) RETURNS void AS \$\$
+        DECLARE
+            old_parent_level integer;
+            new_parent_level integer;
+        BEGIN
+            IF (COALESCE(OLD_PARENT_ID, 0) = COALESCE(NEW_PARENT_ID, 0)) THEN
+                -- нечего перемещать
+                RETURN;
+            END IF;
             
-                DELETE FROM category_tree
-                WHERE
-                    -- удаляем все дочерние связи к NEW.id
-                    child_id IN (
-                        SELECT child_id FROM category_tree WHERE category_id = NEW.id
-                    )
-                    -- и только для того что выше по дереву
-                    AND category_id NOT IN (
-                        SELECT child_id FROM category_tree WHERE category_id = NEW.id
-                    )
-                    -- т.е. внутреннюю вложенность не удаляем
-                ;
+            -- запрещаем рекурсивную привязку друг к другу
+            IF (NEW_PARENT_ID IN (SELECT child_id FROM category_tree WHERE category_id = NEW_CATEGORY_ID)) THEN
+                RAISE EXCEPTION 'Category % is a child of category %', NEW_PARENT_ID, NEW_CATEGORY_ID;
+            END IF;
+        
+            -- уровень вложенности нового родителя
+            SELECT MAX(level) INTO new_parent_level FROM (
+                (SELECT 0 AS level)
+                UNION
+                (SELECT level FROM category_tree WHERE category_id = new_parent_id AND child_id = new_parent_id LIMIT 1)
+            ) t;
                 
-                -- в новое верхнее дерево добавляем дочерние связи
-                INSERT INTO category_tree (category_id, child_id, level)
-                SELECT super_tree.category_id, sub_tree.child_id, sub_tree.level - old_parent_level + new_parent_level
-                FROM category_tree AS super_tree
-                CROSS JOIN category_tree AS sub_tree
+            -- предыдущий уровень вложенности
+            SELECT MAX(level) INTO old_parent_level FROM (
+                (SELECT 0 AS level)
+                UNION
+                (SELECT level FROM category_tree WHERE category_id = OLD_PARENT_ID AND child_id = OLD_PARENT_ID LIMIT 1)
+            ) t;
+            
+           DELETE FROM category_tree
                 WHERE
-                    super_tree.child_id = NEW.parent_id
-                    AND sub_tree.category_id = NEW.id
-                ;
+                -- удаляем все дочерние связи к NEW_CATEGORY_ID
+                child_id IN (
+                    SELECT child_id FROM category_tree WHERE category_id = NEW_CATEGORY_ID
+                )
+                -- и только для того что выше по дереву
+                AND category_id NOT IN (
+                    SELECT child_id FROM category_tree WHERE category_id = NEW_CATEGORY_ID
+                )
+                -- т.е. внутреннюю вложенность не удаляем
+           ;
                 
-                -- обновляем уровень вложенности нетронутых связей
-                UPDATE category_tree
-                SET level = level - old_parent_level + new_parent_level
-                WHERE category_id IN (
-                    SELECT child_id FROM category_tree WHERE category_id = NEW.id
-                );
+           -- в новое верхнее дерево добавляем дочерние связи
+           INSERT INTO category_tree (category_id, child_id, level)
+           SELECT super_tree.category_id, sub_tree.child_id, sub_tree.level - old_parent_level + new_parent_level
+           FROM category_tree AS super_tree
+           CROSS JOIN category_tree AS sub_tree
+           WHERE
+                super_tree.child_id = NEW_PARENT_ID
+                AND sub_tree.category_id = NEW_CATEGORY_ID
+           ;
+                
+           -- обновляем уровень вложенности нетронутых связей
+           UPDATE category_tree
+           SET level = level - old_parent_level + new_parent_level
+           WHERE category_id IN (
+                SELECT child_id FROM category_tree WHERE category_id = NEW_CATEGORY_ID
+           );
+        END;
+        \$\$ LANGUAGE plpgsql
+        ");
+
+        $this->addSql("
+        CREATE OR REPLACE FUNCTION change_parent_category() RETURNS TRIGGER AS \$\$
+        BEGIN
+            IF (TG_OP = 'INSERT') THEN
+                EXECUTE add_category_tree(NEW.id, NEW.parent_id);
+            ELSIF (TG_OP = 'UPDATE') THEN
+                EXECUTE move_category_tree(NEW.id, OLD.parent_id, NEW.parent_id);
             END IF;
             
             RETURN NULL;
@@ -105,6 +127,7 @@ final class Version20220718175837 extends AbstractMigration
     public function down(Schema $schema): void
     {
         $this->addSql('DROP FUNCTION change_parent_category() CASCADE');
+        $this->addSql('DROP FUNCTION move_category_tree');
         $this->addSql('DROP FUNCTION add_category_tree');
     }
 }
