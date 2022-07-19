251ef5ba0ff45d5d989063c7b1e3ac7feec46987
Sergey Zubrilin <serginhold.box@gmail.com>
2022-07-19 16:14:29 +0300

    fuck off

---
+++ src/ConsoleApplication.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+
+class ConsoleApplication extends Application
+{
+    public function getLongVersion(): string
+    {
+        return
+            $this->getSymfonyLongVersion()
+            . sprintf(
+                ' (env: <comment>%s</>, debug: <comment>%s</>)',
+                $this->getKernel()->getEnvironment(),
+                $this->getKernel()->isDebug() ? 'true' : 'false'
+            )
+        ;
+    }
+
+    private function getSymfonyLongVersion(): string
+    {
+        if ('UNKNOWN' !== $this->getName()) {
+            if ('UNKNOWN' !== $this->getVersion()) {
+                return sprintf('%s <info>%s</info>', $this->getName(), $this->getVersion());
+            }
+
+            return $this->getName();
+        }
+
+        return 'Console Tool';
+    }
+}
