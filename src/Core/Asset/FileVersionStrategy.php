35d4df71116f11bbed8b5d23dca3f2b3e0b375d2
Sergey Zubrilin <serginhold.box@gmail.com>
2023-07-12 01:55:09 +0300

    asset version

---
+++ src/Core/Asset/FileVersionStrategy.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core\Asset;
+
+use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
+
+class FileVersionStrategy implements VersionStrategyInterface
+{
+    public function getVersion(string $path): string
+    {
+        return (string)filemtime($path);
+    }
+
+    public function applyVersion(string $path): string
+    {
+        return $path . '?v=' . $this->getVersion($path);
+    }
+}
