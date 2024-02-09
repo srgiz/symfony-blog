8e830a048eae04e98543e0ef995c7b17c9f88446
Sergey Zubrilin <serginhold.box@gmail.com>
2024-02-10 00:20:48 +0300

    JsonRpcResponseException

---
+++ bundles/jsonrpc/src/Exception/JsonRpcResponseException.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SerginhoLD\JsonRpcBundle\Exception;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class JsonRpcResponseException extends \Exception
+{
+    private Response $response;
+
+    public function __construct(Response $response, ?\Throwable $previous = null)
+    {
+        parent::__construct(code: $response->getStatusCode(), previous: $previous);
+        $this->response = $response;
+    }
+
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
+}
